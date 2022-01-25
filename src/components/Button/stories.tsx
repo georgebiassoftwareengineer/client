6608be3b132fcef5c0a1574a52779196918e4d6d
georgebiassoftwareengineer <georgebiassoftwareengineer@gmail.com>
2022-01-24 23:34:15 -0300

    Add Button support for icons

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 import Button from '.'
 
 export default {
@@ -7,6 +8,9 @@ export default {
   argTypes: {
     children: {
       type: 'string'
+    },
+    icon: {
+      type: 'symbol'
     }
   }
 } as Meta
@@ -15,4 +19,12 @@ export const Default: Story = (args) => <Button {...args} />
 
 Default.args = {
   children: 'Buy Now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
 }
