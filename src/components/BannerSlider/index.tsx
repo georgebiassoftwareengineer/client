e5cd6df6f578d23be6015b5d66d45802ffa9f9ff
georgebiassoftwareengineer <georgebiassoftwareengineer@gmail.com>
2022-02-05 00:40:40 -0300

    Add BannerSlider logic and tests

---
+++ src/components/BannerSlider/index.tsx
@@ -0,0 +1,37 @@
+import Banner, { BannerProps } from 'components/Banner'
+import Slider, { SliderSettigns } from 'components/Slider'
+
+import * as S from './styles'
+
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const settings: SliderSettigns = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+  ]
+}
+
+const BannerSlider = ({ items }: BannerSliderProps) => (
+  <S.Wrapper>
+    <Slider settings={settings}>
+      {items.map((item) => (
+        <Banner key={item.title} {...item} />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
+
+export default BannerSlider
